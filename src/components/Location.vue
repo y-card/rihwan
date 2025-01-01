7218492016bd5e9eb8c153b809f640de39842130
y-card <zizidnzz@naver.com>
2025-01-01 22:54:06 +0900

    first commit

---
+++ src/components/Location.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import KakaoMap from "./atom/KakaoMap.vue";
+import LocationButton from "./atom/LocationButton.vue";
+import LocationText from "./atom/LocationText.vue";
+import TextCopy from "./atom/TextCopy.vue";
+const props = defineProps({
+  type: String,
+});
+</script>
+
+<template>
+  <KakaoMap />
+  <TextCopy
+    :class="['location_copy', type]"
+    copy-text="대전광역시 유성구 동서대로 639"
+  >
+    <slot>
+      <p><em>아바니 센트럴 부산 더큐브</em></p>
+      <p>부산 남구 전포대로 133 아바니센트럴부산 4층 더큐브</p>
+    </slot>
+  </TextCopy>
+  <div :class="['btn_locations', type]">
+    <LocationButton type="kakao" link="https://kko.kakao.com/Oi5gL5_S1s"
+      ><span class="loc_text">카카오네비</span></LocationButton
+    >
+    <LocationButton type="tmap" link="https://tmap.life/4e41b09c"
+      ><span class="loc_text">티맵</span></LocationButton
+    >
+    <LocationButton type="naver" link="https://naver.me/FsRs4ZfN"
+      ><span class="loc_text">네이버지도</span></LocationButton
+    >
+  </div>
+  <!-- <div :class="['text_locations', type]">
+    <LocationText title="자가용 이용시">
+      <p class="text">
+        서대전IC, 유성IC에서 10분거리로 1300대 동시주차 가능합니다. 주차시간의
+        제한이 없습니다.
+      </p>
+    </LocationText>
+    <LocationText title="KTX 기차 이용시">
+      <p class="text">
+        대전역 1호선(반석행) > 유성온천 6번 출구 > 706버스 > 아이파크시티 하차 >
+        도보 5분 소요
+      </p>
+    </LocationText>
+    <LocationText title="시외 고속버스 이용시">
+      <p class="text">
+        유성 시외 버스터미널 > 312버스 > 홍도 초등학교 하차 > 도보 10분 소요
+      </p>
+    </LocationText> -->
+  <!-- </div> -->
+</template>
+
+<style>
+.btn_locations {
+  display: flex;
+  gap: 4px;
+  margin: 0 auto;
+}
+.text_locations {
+  margin-top: 30px;
+}
+
+.location_copy,
+.text_locations .text {
+  font-family: "NanumSquare", sans-serif;
+  font-size: 12px;
+}
+
+.location_copy em {
+  font-style: normal;
+  font-weight: bold;
+}
+
+.btn_locations .text {
+  font-size: 13px;
+}
+
+.location_copy.default,
+.btn_locations.default .text,
+.text_locations.default .text {
+  font-family: "NanumSquare", sans-serif;
+  font-size: 12px;
+  color: #000;
+  font-weight: 400;
+}
+
+.loc_text {
+  font-size: 11px;
+  font-family: "NanumSquare", sans-serif;
+  color: #222;
+}
+</style>
---
+++ src/components/MainVisual.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import img from "../assets/image/main.png";
+
+const props = defineProps({
+  type: String,
+});
+</script>
+
+<template>
+  <div :class="['main_visual', props.type, 'fade-in']">
+    <img :src="img" class="main" />
+    <p class="main_title">1st birthday</p>
+    <p class="main_name">김리환</p>
+    <p class="description">첫 생일파티에 초대합니다</p>
+  </div>
+</template>
+
+<style scoped>
+.main_visual {
+  position: relative;
+  font-family: "Nanum Myeongjo", serif;
+  text-align: center;
+  min-height: 500px;
+}
+
+.main_visual .main {
+  width: auto;
+  min-height: 500px;
+  max-height: 813px;
+  transform: translateX(-50%);
+  margin-left: 50%;
+}
+
+.main_visual .main_title,
+.main_visual .main_name,
+.main_visual .description {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 30px;
+  margin: 0 auto;
+  color: #beb9a1;
+  text-align: center;
+  font-size: 18px;
+  font-weight: bolder;
+  line-height: 50px;
+}
+.main_visual .main_name {
+  position: absolute;
+  top: 100px;
+  font-size: 52px;
+  letter-spacing: 1px;
+  color: #333;
+  opacity: 0.9;
+  line-height: 60px;
+}
+
+.main_visual .description {
+  top: 159px;
+  color: #333;
+  opacity: 0.8;
+  font-size: 23px;
+}
+
+.subtitle {
+  margin-bottom: 20px;
+  font-size: 20px;
+}
+
+.bottom {
+  position: absolute;
+  bottom: 30px;
+  font-size: 20px;
+  width: 100%;
+}
+
+@media (min-width: var(--breakpoint)) {
+}
+</style>
